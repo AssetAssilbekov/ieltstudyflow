8ac351528e6e2cafd90be0d566c60fda7146e3b1
AssetAssilbekov <88421733+AssetAssilbekov@users.noreply.github.com>
2024-07-24 09:45:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -60,9 +60,11 @@ addEventListener('scroll', e => {
     if(scrollY/innerHeight < 0.5) {
         document.getElementById('logo').style.opacity = '100%';
         document.getElementById('btn').style.opacity = '100%';
+        document.getElementById('btn2').style.display = 'fixed';
     } else {
     document.getElementById('logo').style.opacity = '0%';
     document.getElementById('btn').style.opacity = '0%';
+    document.getElementById('btn2').style.display = 'none';
     }
 });
 
